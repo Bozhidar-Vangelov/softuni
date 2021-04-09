a11afe878b3c71dcd44d6ce402bdd79e34922e7f
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-04-09 13:16:05 +0300

    Syntax,Functions and Statements - Exercise - road-rage.js updated

--- JavaScript-Advanced/JS-Advanced/01. JS-Advanced-Syntax-Functions-and-Statements-Exercise/road-radar.js
+++ JavaScript-Advanced/JS-Advanced/01. JS-Advanced-Syntax-Functions-and-Statements-Exercise/road-radar.js
@@ -20,15 +20,15 @@ function roadRadar(speed, area) {
 
   let status = "";
 
-  if (difference < 20) {
+  if (difference <= 20) {
     status = "speeding";
-  } else if (difference < 40) {
+  } else if (difference <= 40) {
     status = "excessive speeding";
   } else {
     status = "reckless driving";
   }
 
-  if (difference <= 0) {
+  if (speed <= speedLimit) {
     return `Driving ${speed} km/h in a ${speedLimit} zone`;
   } else {
     return `The speed is ${difference} km/h faster than the allowed speed of ${speedLimit} - ${status}`;

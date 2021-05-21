16afa362a8cf8728caa18e4ab6a9c32fdd261a2a
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-05-21 16:11:55 +0200

    JS Advanced DOM Exercise: 04-08

--- JavaScript-Advanced/JS-Advanced/04. JS-Advanced-DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
+++ JavaScript-Advanced/JS-Advanced/04. JS-Advanced-DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,15 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
+  const input = document.querySelector("#searchField");
+  const rows = document.querySelectorAll("tbody tr");
 
-   function onClick() {
-      //   TODO:
-
-   }
+  function onClick() {
+    for (let row of rows) {
+      if (row.textContent.includes(input.value)) {
+        row.setAttribute("class", "select");
+      } else {
+        row.removeAttribute("class");
+      }
+    }
+  }
 }

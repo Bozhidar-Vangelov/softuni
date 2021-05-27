90ebb56c772ca74ee9a3def53ab8bffb4a66b12d
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-05-27 17:16:42 +0200

    JS Advanced DOM Manipulation: 03-06

--- JavaScript-Advanced/JS-Advanced/05. JS-Advanced-DOM-Events-Lab/03. Delete-From-Table/app.js
+++ JavaScript-Advanced/JS-Advanced/05. JS-Advanced-DOM-Events-Lab/03. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const input = document.querySelector('input[name="email"]').value;
+  const emails = Array.from(
+    document.querySelectorAll("#customers tbody tr td:nth-child(2)")
+  );
+
+  for (let email of emails) {
+    let tr = email.parentNode;
+    if (email.textContent === input) {
+      tr.parentNode.removeChild(tr);
+      document.getElementById("result").textContent = "Deleted.";
+      document.querySelector('input[name="email"]').value = "";
+      return;
+    }
+  }
+
+  document.getElementById("result").textContent = "Not found.";
 }
--- JavaScript-Advanced/JS-Advanced/05. JS-Advanced-DOM-Events-Lab/06. Dynamic-Validation/app.js
+++ JavaScript-Advanced/JS-Advanced/05. JS-Advanced-DOM-Events-Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+  document.getElementById("email").addEventListener("change", onChange);
+
+  function onChange(ev) {
+    const email = ev.target.value;
+
+    if (/^[a-z]+@[a-z]+\.[a-z]+$/.test(email)) {
+      ev.target.classList.remove("error");
+    } else {
+      ev.target.classList.add("error");
+    }
+  }
 }

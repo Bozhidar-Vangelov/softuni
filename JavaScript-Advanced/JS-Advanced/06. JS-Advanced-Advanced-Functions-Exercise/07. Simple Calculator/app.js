64506efb3ffb97f014e58d05c27e028c04db2a95
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-06-14 14:14:03 +0200

    JS Advanced, Functions Advanced: 03-09

--- JavaScript-Advanced/JS-Advanced/06. JS-Advanced-Advanced-Functions-Exercise/07. Simple Calculator/app.js
+++ JavaScript-Advanced/JS-Advanced/06. JS-Advanced-Advanced-Functions-Exercise/07. Simple Calculator/app.js
@@ -1,7 +1,26 @@
 function calculator() {
-    // TODO:
+  let firstElement;
+  let secondElement;
+  let resultElement;
+
+  return {
+    init: (selectorOne, selectorTwo, resultSelector) => {
+      firstElement = document.querySelector(selectorOne);
+      secondElement = document.querySelector(selectorTwo);
+      resultElement = document.querySelector(resultSelector);
+    },
+
+    add: () => {
+      resultElement.value =
+        Number(firstElement.value) + Number(secondElement.value);
+    },
+
+    subtract: () => {
+      resultElement.value =
+        Number(firstElement.value) - Number(secondElement.value);
+    },
+  };
 }
 
-
-
-
+const calculate = calculator();
+calculate.init("#num1", "#num2", "#result");
--- JavaScript-Advanced/JS-Advanced/06. JS-Advanced-Advanced-Functions-Exercise/08. Next Article/script.js
+++ JavaScript-Advanced/JS-Advanced/06. JS-Advanced-Advanced-Functions-Exercise/08. Next Article/script.js
@@ -1,3 +1,9 @@
 function getArticleGenerator(articles) {
-    // TODO
+  let container = $("div#content");
+
+  return function () {
+    if (articles.length > 0) {
+      container.append($("<article>").text(articles.shift()));
+    }
+  };
 }

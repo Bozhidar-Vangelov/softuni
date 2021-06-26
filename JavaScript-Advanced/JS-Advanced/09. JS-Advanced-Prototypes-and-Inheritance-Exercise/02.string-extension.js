52973b80e29679f35b2373ca431513fc172cf7dd
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-06-26 12:06:22 +0200

    JS Advanced - all tasks numbered

---
+++ JavaScript-Advanced/JS-Advanced/09. JS-Advanced-Prototypes-and-Inheritance-Exercise/02.string-extension.js
@@ -0,0 +1,78 @@
+(function () {
+  String.prototype.ensureStart = function (str) {
+    if (this.startsWith(str)) {
+      return this.toString();
+    }
+
+    return str + this.toString();
+  };
+
+  String.prototype.ensureEnd = function (str) {
+    if (this.endsWith(str)) {
+      return this.toString();
+    }
+
+    return this + str;
+  };
+
+  String.prototype.isEmpty = function () {
+    return this.toString() === "";
+  };
+
+  String.prototype.truncate = function (n) {
+    //this is shorter than or equal to n
+    if (this.length <= n) {
+      return this.toString();
+    }
+    //this includes spaces
+    if (this.includes(" ")) {
+      let words = this.split(" ");
+
+      while (words.join(" ").length + 3 > n) {
+        words.pop();
+      }
+
+      return `${words.join(" ")}...`;
+    }
+
+    //this has no spaces
+
+    if (n > 3) {
+      return this.slice(0, n - 3) + "...";
+    }
+
+    return ".".repeat(n);
+  };
+
+  String.format = function (string, ...params) {
+    let replaceRegex = /{(\d+)}/g;
+    let replacedString = string.replace(replaceRegex, (match, group1) => {
+      let index = Number(group1);
+      if (params[index] !== undefined) {
+        return params[index];
+      }
+      return match;
+    });
+    return replacedString;
+  };
+})();
+
+let str = "my string";
+str = str.ensureStart("my");
+console.log(str);
+str = str.ensureStart("hello ");
+console.log(str);
+str = str.truncate(16);
+console.log(str);
+str = str.truncate(14);
+console.log(str);
+str = str.truncate(8);
+console.log(str);
+str = str.truncate(4);
+console.log(str);
+str = str.truncate(2);
+console.log(str);
+str = String.format("The {0} {1} fox", "quick", "brown");
+console.log(str);
+str = String.format("jumps {0} {1}", "dog");
+console.log(str);
